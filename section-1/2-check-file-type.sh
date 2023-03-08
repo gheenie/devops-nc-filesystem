6d3659b50d2aa22f6ebbb1822bbf4fabc96521ae
infoomics <sbx_94@hotmail.com>
2023-03-08 16:05:19 +0000

    add alternative code closer to what the task expected

--- section-1/2-check-file-type.sh
+++ section-1/2-check-file-type.sh
@@ -1,22 +1,42 @@
-read -p "Enter a file/directory name: " USER_INPUT
+read -p "Enter a file/directory name within code folder: " USER_INPUT
 
-FILE_NAME=$(find ./nc-filesystem/code -name ${USER_INPUT}* -type f)
+FILE_OR_FOLDER_NAME=./nc-filesystem/code/${USER_INPUT}
 
-if [[ ${FILE_NAME} != '' ]]; then
+if [ -f ${FILE_OR_FOLDER_NAME} ]; then
     echo ${USER_INPUT} is a file
+
+    exit
 fi
 
-DIRECTORY_NAME=$(find ./nc-filesystem/code -name ${USER_INPUT} -type d)
-
-if [[ ${DIRECTORY_NAME} != '' ]]; then
+if [ -d ${FILE_OR_FOLDER_NAME} ]; then
     echo ${USER_INPUT} is a directory
     echo
     echo Contents of ${USER_INPUT}:
-
-    CONTENTS_IN_MATCHED_DIRECTORY=$(find ${DIRECTORY_NAME}/*)
-
-    for file_or_folder in ${CONTENTS_IN_MATCHED_DIRECTORY}; do
-        # Exclude file path prefix.
-        echo ${file_or_folder##*/}
-    done
+    ls ${FILE_OR_FOLDER_NAME}
+    
+    exit
 fi
+
+echo "${USER_INPUT} doesn't exist"
+
+
+# FILE_NAME=$(find ./nc-filesystem/code -name ${USER_INPUT}* -type f)
+
+# if [[ ${FILE_NAME} != '' ]]; then
+#     echo ${USER_INPUT} is a file
+# fi
+
+# DIRECTORY_NAME=$(find ./nc-filesystem/code -name ${USER_INPUT} -type d)
+
+# if [[ ${DIRECTORY_NAME} != '' ]]; then
+#     echo ${USER_INPUT} is a directory
+#     echo
+#     echo Contents of ${USER_INPUT}:
+
+#     CONTENTS_IN_MATCHED_DIRECTORY=$(find ${DIRECTORY_NAME}/*)
+
+#     for FILE_OR_FOLDER_NAME in ${CONTENTS_IN_MATCHED_DIRECTORY}; do
+#         # Exclude file path prefix.
+#         echo ${FILE_OR_FOLDER_NAME##*/}
+#     done
+# fi
