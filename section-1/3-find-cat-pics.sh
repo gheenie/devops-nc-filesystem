4e7be326b13f740368018ed441593e004466d3d3
infoomics <sbx_94@hotmail.com>
2023-03-08 16:18:32 +0000

    initial code to complete task

--- section-1/3-find-cat-pics.sh
+++ section-1/3-find-cat-pics.sh
@@ -1,1 +1,8 @@
+rm -rf ./section-1/reports
+mkdir -p ./section-1/reports
 
+CAT_FILE_NAMES=$(find ./nc-filesystem -name *.jpeg -type f)
+
+for FILE_NAME in ${CAT_FILE_NAMES}; do
+    echo ${FILE_NAME} >> ./section-1/reports/cat-pics.txt
+done
