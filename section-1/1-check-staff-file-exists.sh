09efeb2cd17cf6b911c31fddfed662bbf30ed9bc
infoomics <sbx_94@hotmail.com>
2023-03-08 15:00:07 +0000

    remove quotes after echo for consistency

--- section-1/1-check-staff-file-exists.sh
+++ section-1/1-check-staff-file-exists.sh
@@ -1,7 +1,7 @@
 FILE_NAME=$(find ./nc-filesystem/staff -name nc-${1}.json)
 
 if [[ ${FILE_NAME} != '' ]]; then
-    echo 'staff file found'
+    echo staff file found
 else
-    echo 'no staff file'
+    echo no staff file
 fi
