ec8d291f9405c534bbf480a4f5b8eedf2036875c
infoomics <sbx_94@hotmail.com>
2023-03-08 14:29:27 +0000

    remove quotes for name parameter

--- section-1/1-check-staff-file-exists.sh
+++ section-1/1-check-staff-file-exists.sh
@@ -1,4 +1,4 @@
-FILE_NAME=$(find ./nc-filesystem/staff -name "nc-$1.json")
+FILE_NAME=$(find ./nc-filesystem/staff -name nc-${1}.json)
 
 if [[ $FILE_NAME != '' ]]; then
     echo 'staff file found'
