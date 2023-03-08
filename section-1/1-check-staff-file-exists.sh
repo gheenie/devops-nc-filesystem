afa0b40ec059e66ae53509298a0445b45aac1c54
infoomics <sbx_94@hotmail.com>
2023-03-08 14:37:22 +0000

    add curly braces for consistency

--- section-1/1-check-staff-file-exists.sh
+++ section-1/1-check-staff-file-exists.sh
@@ -1,6 +1,6 @@
 FILE_NAME=$(find ./nc-filesystem/staff -name nc-${1}.json)
 
-if [[ $FILE_NAME != '' ]]; then
+if [[ ${FILE_NAME} != '' ]]; then
     echo 'staff file found'
 else
     echo 'no staff file'
