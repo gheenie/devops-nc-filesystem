4b224eee26f078d02728d2473ae4027e8051fea0
infoomics <sbx_94@hotmail.com>
2023-03-08 15:38:08 +0000

    add code to display directory contents

--- section-1/2-check-file-type.sh
+++ section-1/2-check-file-type.sh
@@ -10,4 +10,13 @@ DIRECTORY_NAME=$(find ./nc-filesystem/code -name ${USER_INPUT} -type d)
 
 if [[ ${DIRECTORY_NAME} != '' ]]; then
     echo ${USER_INPUT} is a directory
+    echo
+    echo Contents of ${USER_INPUT}:
+
+    CONTENTS_IN_MATCHED_DIRECTORY=$(find ${DIRECTORY_NAME}/*)
+
+    for file_or_folder in ${CONTENTS_IN_MATCHED_DIRECTORY}; do
+        # Exclude file path prefix.
+        echo ${file_or_folder##*/}
+    done
 fi
